dfa57aa0bf4d07582e8f72336109380b48c270f4
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-06-04 14:44:33 -0500

    Fixed gap with footer, changed footer text from gray on gray to black on gray

--- rtportfolio/src/components/Footer.js
+++ rtportfolio/src/components/Footer.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <footer className="container-fluid row justify-content-md-start justify-content-center lgray-back position-fixed bottom-0" id="sec-contact" title="Contact-Me">
+        <footer className="container-fluid row justify-content-md-start justify-content-center lgray-back position-fixed bottom-0 m-0" id="sec-contact" title="Contact-Me">
           <div className="d-flex col-12 align-items-center justify-content-center">
             <ul className="nav col-12 flex-wrap text-md-start text-start justify-content-center">
-              <li className="col-lg-2 col-12 fs-3"><a href="mailto:ryan.thomas@utexas.edu" target="_blank" rel="noreferrer" className="nav-link ">Email</a></li>
-              <li className="col-lg-2 col-12 fs-3"><a href="https://github.com/Stiltskinner" target="_blank" rel="noreferrer" className="nav-link ">Github</a></li>
-              <li className="col-lg-2 col-12 fs-3"><a href="https://www.linkedin.com/in/ryanpaxthomas/" target="_blank" rel="noreferrer" className="nav-link ">LinkedIn</a></li>
-              <li className="col-lg-2 col-12 fs-3"><a href="https://twitter.com/Stiltskinner" target="_blank" rel="noreferrer" className="nav-link ">Twitter</a></li>
+              <li className="col-lg-2 col-12 fs-3"><a href="mailto:ryan.thomas@utexas.edu" target="_blank" rel="noreferrer" className="nav-link footer-link">Email</a></li>
+              <li className="col-lg-2 col-12 fs-3"><a href="https://github.com/Stiltskinner" target="_blank" rel="noreferrer" className="nav-link footer-link">Github</a></li>
+              <li className="col-lg-2 col-12 fs-3"><a href="https://www.linkedin.com/in/ryanpaxthomas/" target="_blank" rel="noreferrer" className="nav-link footer-link">LinkedIn</a></li>
+              <li className="col-lg-2 col-12 fs-3"><a href="https://twitter.com/Stiltskinner" target="_blank" rel="noreferrer" className="nav-link footer-link">Twitter</a></li>
             </ul>
           </div>
       </footer>
--- rtportfolio/src/components/PortfolioContainer.js
+++ rtportfolio/src/components/PortfolioContainer.js
@@ -1,5 +1,3 @@
-// TODO: Replace imports for Portfolio with portfolio and blog with resume. Most like replace navtabs with header
-
 import React, { useState } from 'react';
 import Header from './Header';
 import Portfolio from './pages/Portfolio';
@@ -7,12 +5,6 @@ import About from './pages/About';
 import Resume from './pages/Resume';
 import Contact from './pages/Contact';
 import Footer from './Footer';
-
-// General TODO List
-// Set up custom CSS to work.
-// Import existing CSS from my last portfolio
-// Update existing CSS to use a white background/new color palette
-// Create a Project component to reuse for portfolio
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('About');
@@ -34,8 +26,6 @@ export default function PortfolioContainer() {
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
-
-// TODO Create a consistent header and put navigation inside of it. Most likely replace navtabs with header
 
   return (
     <div>
